045c33f1919030ba958233ccf1ee03f4872438e9
Peter Murphy <peter.francis.murphy@gmail.com>
2019-03-18 15:20:56 -0400

    Adds post method to API

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// implement your API here
+const express = require("express")
+const db = require("./data/db")
+
+const app = express()
+app.use(express.json())
+
+app.post("/api/users", (req, res) => {
+  const { name, bio } = req.body
+  const now = new Date()
+  if (name == null || bio == null) {
+    res.status(400).json({
+      errorMessage: "Please provide name and bio for the user."
+    })
+  } else {
+    db.insert({
+      name,
+      bio,
+      created_at: now,
+      updated_at: now
+    })
+      .then(({ id }) => {
+        res.status(201).json({
+          id,
+          name,
+          bio,
+          created_at: now,
+          updated_at: now
+        })
+      })
+      .catch(() => {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        })
+      })
+  }
+})
+
+app.listen(4000, () => {
+  console.log("\n** API up and running on port 4000 **")
+})
